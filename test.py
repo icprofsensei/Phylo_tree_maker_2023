f555ad53c8ae6f36ce7027d8412f1e452b998ee1
Dhylan Patel <dp521@ic.ac.uk>
2024-04-23 20:51:12 +0100

    minor

--- test.py
+++
@@ -1,9 +0,0 @@
-a= ['p', 'p', 'q', 'r ', 's', 't', 'y', 'v', 'p', 'p', 'q','p', 'p', 'q','p', 'p', 'q']
-testdict = dict()
-print(testdict)
-aset = set(a)
-for itf in aset:
-    testdict[itf] = 0
-for itf in a:
-    testdict[itf] += 1
-print(testdict)
